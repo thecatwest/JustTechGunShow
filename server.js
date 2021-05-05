fc5a589dfa0ce86177a039dd2ea42af40f353ddb
TheCatWest <catwestover@outlook.com>
2021-05-05 14:44:26 -0700

    front end file structure

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
-// test 
-// testing 
-// 1 2  3 
-// test 2
+const path = require('path');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers/');
+const hbs = exphbs.create({});
+
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
