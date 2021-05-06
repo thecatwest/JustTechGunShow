df29f4bf209f57ca39e27ac21d9161dcb9fa6b3f
claire ogesen <claire.ogesen.co@gmail.com>
2021-05-06 11:31:05 -0700

    modles and things

--- models/index.js
+++ models/index.js
@@ -1,15 +1,51 @@
 // import all models
+const Post = require('./Post');
 const User = require('./User');
-conts Post = require('./Post'); 
+const Comment = require('./Comment');
+const Category = require('./Category');
+// 
 
-// create association 
+// create association user can have many posts
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+  });
+  
+  // a post can only have one user 
+  Post.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
 
+//models associations for Comment 
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
 
+   //models associations for Comment 
+   Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
 
-//coment to post 
-// post belong to users 
+  Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+  });
 
+  User.hasMany(Comment, {
+    foreignKey: 'user_id'
+  });
+
+  Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+  });
+
+//category 
+Category.hasMany(Post, {
+    foreignKey: 'post_id'
+}); 
+
+Post.belongsTo(Category, {
+    foreignKey: 'category_id'
+});
 
 
 // export all models 
-module.exports = { User, Post };
+module.exports = { User, Post, Comment, Category };
