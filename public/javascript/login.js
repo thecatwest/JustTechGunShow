da6abfc1feaa0f6da654203a13505f9ecc3824d1
TheCatWest <catwestover@outlook.com>
2021-05-09 09:31:00 -0700

    login setup

---
+++ public/javascript/login.js
@@ -0,0 +1,56 @@
+// event listener for submit event from signup form
+async function signupFormHandler(event) {
+  event.preventDefault();
+
+  const username = document.querySelector("#username-signup").value.trim();
+  const email = document.querySelector("#email-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
+
+  //   if all fields contain values, make post request
+  if (username && email && password) {
+    const response = await fetch("/api/users", {
+      method: "post",
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    // check response status
+    if (response.ok) {
+      console.log('success');
+    //   unsuccessful response triggers error
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+
+// event listener for login event from login form
+async function loginFormHandler(event) {
+    event.preventDefault();
+  
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+  
+    if (email && password) {
+      const response = await fetch('/api/users/login', {
+        method: 'post',
+        body: JSON.stringify({
+          email,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  }
+  
+  document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
