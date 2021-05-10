731294fad9fbcc346e51f5643a30aa1bd6b66f54
Big Head <spencer.moyer16@yahoo.com>
2021-05-10 14:40:41 -0700

    finished user routes

--- models/Post.js
+++ models/Post.js
@@ -5,39 +5,49 @@ class Post extends Model {}
 
 // create fields/columns for Post model
 Post.init(
-    {
+  {
       id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true
       },
       title: {
-        type: DataTypes.STRING,
-        allowNull: false
+          type: DataTypes.STRING,
+          allowNull: false
       },
       post_url: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          isURL: true
-        }
+          type: DataTypes.STRING,
+          allowNull: false,
+          validate: {
+              isUrl: true
+          }
       },
       user_id: {
-        type: DataTypes.INTEGER,
-        references: {
-          model: 'user',
-          key: 'id'
-        }
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          references: {
+              model: 'user',
+              key: 'id'
+          }
+      },
+      category_id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          references: {
+            model: 'category',
+            key: 'id'
+          }
       }
-    },
-    {
+  },
+  {
       sequelize,
       freezeTableName: true,
       underscored: true,
       modelName: 'post'
-    }
-  );
+  }
+);
+
   
   module.exports = Post;
   
