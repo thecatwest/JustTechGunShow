f2155fed1e9e05b743848fb7d71a3e3a49af8998
claire ogesen <claire.ogesen.co@gmail.com>
2021-05-09 19:43:14 -0700

    post clean up

--- models/Post.js
+++ models/Post.js
@@ -1,38 +1,7 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Post extends Model {
-    // this is no joke copied words 
-    static upvote(body, models) {
-        return models.Vote.create({
-          user_id: body.user_id,
-          post_id: body.post_id
-        }).then(() => {
-          return Post.findOne({
-            where: {
-              id: body.post_id
-            },
-            attributes: [
-              'id',
-              'post_url',
-              'title',
-              'created_at',
-              [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-            ],
-            include: [
-              {
-                model: models.Comment,
-                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-                include: {
-                  model: models.User,
-                  attributes: ['username']
-                }
-              }
-            ]
-          });
-        });
-      }
-}
+class Post extends Model {}
 
 // create fields/columns for Post model
 Post.init(
