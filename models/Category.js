59412c33219eeb312b2d8c05d1289410eaa2ac2a
TheCatWest <catwestover@outlook.com>
2021-05-10 19:19:49 -0700

    stuff and things

--- models/Category.js
+++ models/Category.js
@@ -20,13 +20,7 @@ Category.init(
       validate: {
         len: [1],
       },
-      category_description: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [1]
-        }
-      }
+    },
   },
   {
     sequelize,
