5718f4a781aa1b99adf047bcbb159a998e31d597
TheCatWest <catwestover@outlook.com>
2021-05-10 17:57:32 -0700

    final partials and views

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -8,11 +8,11 @@ test("format_date() returns a date string", () => {
 });
 
 test("format_plural() returns a pluralized word", () => {
-  const word1 = format_plural("tiger", 1);
-  const word2 = format_plural("lion", 2);
+  const word1 = format_plural("cat", 1);
+  const word2 = format_plural("dogs", 2);
 
-  expect(word1).toBe("tiger");
-  expect(word2).toBe("lions");
+  expect(word1).toBe("cat");
+  expect(word2).toBe("dogs");
 });
 
 test("format_url() returns a simplified url string", () => {
