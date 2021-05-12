ff7b224f8f03644acc4a50102fe56360f6a81bb9
claire ogesen <claire.ogesen.co@gmail.com>
2021-05-12 16:54:33 -0700

    yt not working

--- public/javascript/video.js
+++ public/javascript/video.js
@@ -1,75 +1,23 @@
-const ytForm = document.getElementById('yt-form');
-const keywordInput = document.getElementById('keyword-input');
-const maxresultInput = document.getElementById('maxresult-input');
-const orderInput = document.getElementById('order-input');
-const videoList = document.getElementById('videoListContainer');
-var pageToken = '';
-  
-gapi.load("client", loadClient);
-  
-function loadClient() {
-    gapi.client.setApiKey("AIzaSyBRqNr_DoBpN-1JZDJymfAVPPEw0juYfnY");
-    return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
-        .then(function() { console.log("GAPI client loaded for API"); },
-                function(err) { console.error("Error loading GAPI client for API", err); });
-}
 
-ytForm.addEventListener('submit', e => {
-    e.preventDefault();
-    execute();
-});
-  
-function paginate(e, obj) {
-    e.preventDefault();
-    pageToken = obj.getAttribute('data-id');
-    execute();
-}
-  
-// Make sure the client is loaded before calling this method.
-function execute() {
-    const searchString = keywordInput.value.trim;
-    const maxresult = maxresultInput.value.trim;
-    const orderby = orderInput.value.trim;
-  
-    var arr_search = {
-        "part": 'snippet',
-        "type": 'video',
-        "order": orderby,
-        "maxResults": maxresult,
-        "q": searchString
-    };
-  
-    if (pageToken != '') {
-        arr_search.pageToken = pageToken;
-    }
-  
-    return gapi.client.youtube.search.list(arr_search)
-    .then(function(response) {
-        // Handle the results here (response.result has the parsed body).
-        const listItems = response.result.items;
-        if (listItems) {
-            let output = '<h4>Videos</h4><ul>';
-  
-            listItems.forEach(item => {
-                const videoId = item.id.videoId;
-                const videoTitle = item.snippet.title;
-                output += `
-                    <li><a data-fancybox href="https://www.youtube.com/watch?v=${videoId}"><img src="http://i3.ytimg.com/vi/${videoId}/hqdefault.jpg" /></a><p>${videoTitle}</p></li>
-                `;
-            });
-            output += '</ul>';
-  
-            if (response.result.prevPageToken) {
-                output += `<br><a class="paginate" href="#" data-id="${response.result.prevPageToken}" onclick="paginate(event, this)">Prev</a>`;
-            }
-  
-            if (response.result.nextPageToken) {
-                output += `<a href="#" class="paginate" data-id="${response.result.nextPageToken}" onclick="paginate(event, this)">Next</a>`;
-            }
-  
-            // Output list
-            videoList.innerHTML = output;
-        }
-    },
-    function(err) { console.error("Execute error", err); });
-}
+// require google apis package "npm i googleapis"
+const { google } = require('googleapis');
+//AIzaSyBRqNr_DoBpN-1JZDJymfAVPPEw0juYfnY - api key
+require('dotenv').config();
+
+const ytForm = document.getElementById('#yt-form');
+
+google.youtube('v3').search.list({
+    key: process.env.YOUTUBE_TOKEN,
+    part: 'snippet',
+    q: 'motivation',
+    maxResults: 5,
+
+}).then((response) => {
+    const { data } = response;
+    data.items.forEach((item) => {
+        console.log(`Title: ${item.snippet.title}\nDescription:${item.snippet.description}\n`);
+
+    })
+}).catch((err) => console.log(err));
+
+ytForm.addEventListener("submit")
